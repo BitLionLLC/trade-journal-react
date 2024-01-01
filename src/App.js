d4b8567b772ac7092eb7fffa1f23c17a70502f82
Grant Reighard <grant.reighard@gmail.com>
2024-01-01 16:25:26 -0500

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [tradeList, setTradeList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/get-trades`)
+      .then((res) => {
+        setTradeList(res.data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const formatSeconds = (seconds) => {
+    let secondsLeft = seconds;
+    const hours = Math.floor(secondsLeft / 3600);
+    secondsLeft -= hours * 3600;
+    const minutes = Math.floor(secondsLeft / 60);
+    secondsLeft -= minutes * 60;
+
+    return `${hours}h ${minutes}m ${Math.round(secondsLeft)}s`;
+  };
+
+  const instrumentMap = {
+    306: "NAS100",
+    307: "SPX500",
+    309: "US30",
+    314: "XAUUSD",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Order #</th>
+            <th>Instrument</th>
+            <th>Date Opened</th>
+            <th>Time Opened</th>
+            <th>Date Closed</th>
+            <th>Time Closed</th>
+            <th>Duration of Trade</th>
+            <th>Lots</th>
+            <th>Direction</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tradeList.map((trade) => {
+            return (
+              <tr>
+                <td>{trade[0]}</td>
+                <td>{instrumentMap[trade[1]]}</td>
+                <td>
+                  {new Date(parseInt(trade[13])).toLocaleDateString("en-US")}
+                </td>
+                <td>
+                  {new Date(parseInt(trade[13])).toLocaleTimeString("en-US")}
+                </td>
+                <td>
+                  {new Date(parseInt(trade[14])).toLocaleDateString("en-US")}
+                </td>
+                <td>
+                  {new Date(parseInt(trade[14])).toLocaleTimeString("en-US")}
+                </td>
+                <td>
+                  {formatSeconds(
+                    Math.abs(parseInt(trade[14]) - parseInt(trade[13])) / 1000
+                  )}
+                </td>
+                <td>{trade[3]}</td>
+                <td>{trade[4]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
